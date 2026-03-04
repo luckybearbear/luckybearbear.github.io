f4dbf6b6624f55feee6fdf575e5988f2f4c277bb
luckybearbear <35355954+luckybearbear@users.noreply.github.com>
2026-03-04 17:22:18 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/luckubearbear",
+      GitHub: "https://github.com/luckybearbear",
       // "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
