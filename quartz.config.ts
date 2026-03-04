7d16f1087b5be6a455619adc9758ec8e2d7bd10b
luckybearbear <35355954+luckybearbear@users.noreply.github.com>
2026-03-04 09:59:56 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "https://luckybearbear.github.io",
+    baseUrl: "luckybearbear.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
