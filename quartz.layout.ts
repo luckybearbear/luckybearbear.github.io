134f080867454549132686536ebe4fc8fde20fa1
luckybearbear <jun.zhang@pexetech.com>
2026-03-06 17:01:32 +0800

    1

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,33 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "luckybearbear/quartz",
+        // from data-repo-id
+        repoId: "R_kgDOQKerxg",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOQKerxs4C3y7l",
+        // how to map pages -> discussions
+        // defaults to 'url'
+        mapping: "pathname",
+        // use strict title matching
+        // defaults to true
+        strict: false,
+        // whether to enable reactions for the main post
+        // defaults to true
+        reactionsEnabled: true,
+        // where to put the comment input box relative to the comments
+        // defaults to 'bottom'
+        inputPosition: "top",
+      },
+    })
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/luckybearbear",
