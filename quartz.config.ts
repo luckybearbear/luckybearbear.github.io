942671b6d2c55755b4a2246de74e7fa99d707227
luckybearbear <jun.zhang@pexetech.com>
2026-03-06 10:00:22 +0800

    更新配置

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "熊熊的博客",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "luckybearbear.github.io",
+    locale: "zh-CN",
+    baseUrl: "luckybearbear.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
